1641cb68d9502098d9a9783a7694cca8c46178ee
Alexander Smirnov <al.smirnov996@gmail.com>
2018-07-26 13:57:58 +0300

    Create base structure of application

---
+++ src/components/Root/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Content from '../Content/index';
+
+export default class Root extends React.Component {
+    render() {
+        return (
+            <div>
+                <Content/>
+            </div>
+        )
+    }
+}
