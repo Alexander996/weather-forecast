9a4a79cf83c324ce2aa7e39f33d4fdba193eeec6
Alexander Smirnov <al.smirnov996@gmail.com>
2018-07-26 14:21:59 +0300

    Add redux

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from 'react-redux'
 
 import Content from '../Content/index';
+import store from '../../store'
 
 export default class Root extends React.Component {
     render() {
         return (
-            <div>
+            <Provider store={store}>
                 <Content/>
-            </div>
+            </Provider>
         )
     }
 }
